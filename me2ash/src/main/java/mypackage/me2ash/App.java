a20691e8972f9caa17381bbe240ee6a0e4b01a95
ASWIN VIJAY <aswinvi@gmail.com>
2017-05-31 10:50:39 +0530

    new project change

---
+++ me2ash/src/main/java/mypackage/me2ash/App.java
@@ -0,0 +1,13 @@
+package mypackage.me2ash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
